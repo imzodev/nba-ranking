13ca391e522cae87a26ee4eb21faae3f3576489b
Irving Zamora <imzodev@gmail.com>
2025-08-01 05:09:28 -0600

    feat: add Vercel Analytics to track site usage and performance

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import ThemeProvider from "../components/ui/ThemeProvider";
 import CookieConsent from "../components/ui/CookieConsent";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -57,6 +58,7 @@ export default function RootLayout({
           {children}
           <CookieConsent />
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   );
